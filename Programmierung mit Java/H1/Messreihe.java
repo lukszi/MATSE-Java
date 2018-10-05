dd48e6392ccdc093ff125a3b72e084eb00de03fc
Felix Sz <Felix.szimtenings@gmail.com>
2018-10-05 19:19:43 +0200

    Merge branch 'master' of https://github.com/lukszi/MATSE
    # Conflicts:
    #	Programmierung mit Java/H1/Messreihe.java
    
    Methode getMin() erstellt.

--- Programmierung mit Java/H1/Messreihe.java
+++ Programmierung mit Java/H1/Messreihe.java
@@ -19,7 +19,14 @@ public class Messreihe
 
     public double getMin()
     {
-        double ret = 0.0;
+        boolean a = true;
+        double ret = 0;
+        for (int i= 0; i<data.length-1; i++)  {
+            if(data[i] < ret || a == true) {
+              ret = data[i];
+              a = false;
+            }
+        }
         return ret;
     }
 
@@ -56,7 +63,7 @@ public class Messreihe
 
     public int getAnzahl()
     {
-        int count = 0;
+        int count = data.length;
         return count;
     }
 }
