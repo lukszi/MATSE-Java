9bfa755365b7428a20eb24d9ee7fd0abf138ce11
ls440063 <lukas.szimtenings@rwth-aachen.de>
2018-10-05 18:35:06 +0200

    Ein paar Funktionsrümpfe hinzugefügt

--- Programmierung mit Java/H1/Messreihe.java
+++ Programmierung mit Java/H1/Messreihe.java
@@ -1,3 +1,44 @@
 public class Messreihe
 {
+    private double data[];
+
+    public Messreihe(double[] list)
+    {
+
+    }
+
+    public Messreihe(String fileName)
+    {
+
+    }
+
+    public double getMax()
+    {
+        double ret = 0.0;
+        return ret;
+    }
+
+    public double getMin()
+    {
+        double ret = 0.0;
+        return ret;
+    }
+
+    public boolean istEinheitlich()
+    {
+        boolean ret = true;
+        return ret;
+    }
+
+    public int zaehleGroessere(double lim)
+    {
+        int count = 0;
+        return count;
+    }
+
+    public int getMaxIndex()
+    {
+        int i = 0;
+        return i;
+    }
 }
