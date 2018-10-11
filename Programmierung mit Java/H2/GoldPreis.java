2891b0ee8cc0d6046c822e8d81c15fdf1e9432b8
ls440063 <lukas.szimtenings@rwth-aachen.de>
2018-10-11 10:57:52 +0200

    added Homework files
    added fields and methods as called for in the homework files

--- Programmierung mit Java/H2/GoldPreis.java
+++ Programmierung mit Java/H2/GoldPreis.java
@@ -1,4 +1,18 @@
+import java.util.ArrayList;
+
 public class GoldPreis
 {
+    private ArrayList<GoldTagesPreis> list;
 
+    public GoldPreis(String dateiname){
+
+    }
+
+    public String getPreis(String datum) throws NumberFormatException{
+        return "";
+    }
+
+    public void printMinMax(){
+
+    }
 }
--- Programmierung mit Java/H2/GoldTagesPreis.java
+++ Programmierung mit Java/H2/GoldTagesPreis.java
@@ -1,3 +1,20 @@
 public class GoldTagesPreis
 {
+    public String datum;
+    public double preis;
+
+    public GoldTagesPreis(String datum, double preis)
+    {
+        this.datum = datum;
+        this.preis = preis;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "GoldTagesPreis{" +
+                "datum='" + datum + '\'' +
+                ", preis=" + preis +
+                '}';
+    }
 }
