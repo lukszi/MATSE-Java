7f882c161369e9eda3ece4873f93754ca5b82a1f
ls440063 <lukas.szimtenings@rwth-aachen.de>
2018-11-07 13:41:28 +0100

    Zusatzaufgabe erledigt

--- Programmierung mit Java/PA6/BenutzerFormat.java
+++ Programmierung mit Java/PA6/BenutzerFormat.java
@@ -1,14 +1,22 @@
+import java.util.Locale;
+
 public class BenutzerFormat implements BruchFormat
 {
+    private Locale locale;
     private String format;
 
-    public BenutzerFormat(String format){
+    public BenutzerFormat(String format, Locale locale){
         this.format = format;
+        this.locale = locale;
+    }
+
+    public BenutzerFormat(String format){
+        this(format, Locale.GERMAN);
     }
 
     @Override
     public String bruchToString(int zaehler, int nenner)
     {
-        return String.format(format,(double)zaehler/nenner);
+        return String.format(format,(double)zaehler/nenner, locale);
     }
 }
