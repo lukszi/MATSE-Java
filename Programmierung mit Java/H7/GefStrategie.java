767b3fcb3575e53f0b3629aab543f7e2e21854c2
Zahnrad1337 <43889008+Zahnrad1337@users.noreply.github.com>
2018-11-20 08:36:13 +0100

    Update GefStrategie.java

--- Programmierung mit Java/H7/GefStrategie.java
+++ Programmierung mit Java/H7/GefStrategie.java
@@ -1,5 +1,15 @@
+/**
+ * @author Lukas Szimtenings
+ * @author Felix Szimtenings
+ * @author Sebastian Schmitz
+ */
 public interface GefStrategie
 {
+    /**
+    * implements a certain decision for the player
+    * @param boolean is used 
+    * @return next decision of the player
+    */
     boolean getNextDecision();
 
     void setOpponentNextDecision(boolean decision);
