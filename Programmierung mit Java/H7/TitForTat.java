75c23e2aa3035c2f45efc964cf7e60f05cd8b3e4
Zahnrad1337 <43889008+Zahnrad1337@users.noreply.github.com>
2018-11-20 08:51:46 +0100

    Update TitForTat.java

--- Programmierung mit Java/H7/TitForTat.java
+++ Programmierung mit Java/H7/TitForTat.java
@@ -1,12 +1,23 @@
+/**
+ * @author Lukas Szimtenings
+ * @author Felix Szimtenings
+ * @author Sebastian Schmitz
+ */
 public class TitForTat implements GefStrategie
 {
     private boolean oponentDecision = true;
+    /**
+    * @param decision from GefStragie
+    * @return nexDecision
+    */
     @Override
     public boolean getNextDecision()
     {
         return this.oponentDecision;
     }
-
+    /**
+    * @param decision
+    */
     @Override
     public void setOpponentNextDecision(boolean decision)
     {
