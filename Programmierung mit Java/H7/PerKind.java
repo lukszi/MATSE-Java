ab3e26456b848415f5cf956429837058f46b4c95
Zahnrad1337 <43889008+Zahnrad1337@users.noreply.github.com>
2018-11-20 08:41:56 +0100

    Update PerKind.java

--- Programmierung mit Java/H7/PerKind.java
+++ Programmierung mit Java/H7/PerKind.java
@@ -1,14 +1,24 @@
+/**
+ * @author Lukas Szimtenings
+ * @author Felix Szimtenings
+ * @author Sebastian Schmitz
+ */
 public class PerKind implements GefStrategie
 {
     private int cooperate = 0;
-
+    /**
+    * @param decision from GefStrategie
+    * @return if there will be a cooperation of the player
+    */
     @Override
     public boolean getNextDecision()
     {
         cooperate ++;
         return cooperate%3!=0;
     }
-
+    /**
+    *@param set next decision
+    */
     @Override
     public void setOpponentNextDecision(boolean decision)
     {
