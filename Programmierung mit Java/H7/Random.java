8f9358b4503ea05881ebd433b31032beafb46540
Zahnrad1337 <43889008+Zahnrad1337@users.noreply.github.com>
2018-11-20 08:45:42 +0100

    Update Random.java

--- Programmierung mit Java/H7/Random.java
+++ Programmierung mit Java/H7/Random.java
@@ -1,11 +1,22 @@
+/**
+ * @author Lukas Szimtenings
+ * @author Felix Szimtenings
+ * @author Sebastian Schmitz
+ */
 public class Random implements GefStrategie
 {
+    /**
+    * @param decision from GefStrategie
+    * @return random number of 0 or 1
+    */
     @Override
     public boolean getNextDecision()
     {
         return Math.random()<0.5;
     }
-
+    /**
+    * @param set next decision
+    */
     @Override
     public void setOpponentNextDecision(boolean decision)
     {
